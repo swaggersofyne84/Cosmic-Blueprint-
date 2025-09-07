e577c4b114d6b364b469b570eabe9600ede265e4
swaggersofyne84 <kyle.merritt@cosmicblueprint.space>
2025-09-07 10:16:56 -0700

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,5 @@
-
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req,res)=> res.send('Cosmic Blueprint API running'));
+app.listen(PORT, ()=> console.log(`Server on ${PORT}`));
